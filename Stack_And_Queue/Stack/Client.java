6a44a559f2e4a6e30688bdd3d9ad14ed97f12c55
Harsh1809Dev <dev182000@gmai.com>
2021-03-24 15:30:43 +0530

    Incomplete

---
+++ Stack_And_Queue/Stack/Client.java
@@ -0,0 +1,8 @@
+public class Client{
+    public static void main(String[] args) throws Exception{
+        Stack st = new Stack();
+        for(int i = 0; i < 10 ; i++)
+            st.push(i * 10 + 1);
+        st.push(111);
+    }
+}
---
+++ Stack_And_Queue/Stack/Stack.java
@@ -0,0 +1,72 @@
+public class Stack{
+    protected int[] arr = null;
+    protected int capacity = 0;
+    protected int size = 0;
+    protected int tos = -1;
+
+    Stack(){
+        initialize_variables(10);
+    }
+
+    Stack(int n){
+        initialize_variables(n);
+    }
+
+    protected void initialize_variables(int capacity){
+        this.arr = new int[capacity];
+        this.capacity = capacity;
+        this.size = 0;
+        this.tos = -1;
+    }
+
+    public int size_(){
+        return this.size;
+    }
+
+    public boolean is_empty(){
+        return tos == -1;
+    }
+
+    //exceptions=========================================================
+    private void over_flow_exception() throws Exception{
+        if(this.capacity == this.size)
+            throw new Exception("Stack Already Full");
+    }
+
+    private void under_flow_exception() throws Exception{
+        if(this.tos == -1){
+            throw new Exception("Stack Already Empty");
+        }
+    }
+    //basic_functions====================================================
+    protected void push_(int data){
+        arr[++tos] = data;
+        this.size ++;
+    }
+
+    public void push(int data) throws Exception{
+        over_flow_exception();
+        push_(data);
+    }
+
+    public int pop() throws Exception{
+        under_flow_exception();
+        return pop_();
+    }
+
+    protected int pop_(){
+        int ele = arr[tos];
+        arr[tos--] = 0;
+        this.size--;
+        return ele;
+    }
+
+    public int peek() throws Exception{
+        under_flow_exception();
+        return peek_();
+    }
+
+    protected int peek_(){
+        return arr[tos];
+    }
+}
